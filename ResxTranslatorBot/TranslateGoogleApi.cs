45c2029b01cd3f331afd1e983bac759da9ff872c
Alex <jitbit+github@gmail.com>
2019-05-18 20:52:55 +0300

    Added support for google api (not in production yet)

---
+++ ResxTranslatorBot/TranslateGoogleApi.cs
@@ -0,0 +1,22 @@
+﻿using Google.Cloud.Translation.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResxTranslator
+{
+	public static class TranslateGoogleApi
+	{
+		public static string TranslateText(string input, string targetLanguage)
+		{
+			if (string.IsNullOrWhiteSpace(input)) return "";
+			if (string.IsNullOrWhiteSpace(targetLanguage)) throw new ArgumentException("Target lang not set");
+
+			TranslationClient client = TranslationClient.Create();
+			var response = client.TranslateText(input, targetLanguage, "en");
+			return response.TranslatedText;
+		}
+	}
+}
